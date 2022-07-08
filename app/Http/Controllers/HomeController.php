391870616e21c58635d4bde8ccb0f0594b69b9aa
Fasyaziyan <fasyaziyan@gmail.com>
2022-07-08 13:13:32 +0700

    Dasboard Siswa Chart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use App\Models\Mapel;
 use App\Models\Tingkat;
 use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -60,7 +61,6 @@ class HomeController extends Controller
         foreach ($data_siswa_nama as $item => $value) {
             $data3[] = $value->nama;
         }
-        ////
 
         foreach ($data2 as $item => $value) {
             $siswa_kelas[] = Siswa::leftJoin('detnilai', 'siswa.nisn', '=', 'detnilai.nisn')->where('siswa.nisn', '=', $value)->leftJoin('mapel', 'mapel.id_mapel', '=', 'detnilai.id_mapel')->where('mapel.id_kelas', '=', $request->id_kelas)->where('detnilai.id_kuri', '=', $id_kuri)->get();
@@ -87,6 +87,32 @@ class HomeController extends Controller
         $date = Carbon::now('Asia/Jakarta');
         $date = $date->format('d-m-Y')." ".$date->format('H:i');
 
-        return view('dashboard2', compact('date'));
+        //List Nama Mapel
+        $id_kelas = Auth::guard('siswa')->user()->id_kelas;
+        $list_mapel = Mapel::where('id_kelas', $id_kelas)->get();
+        $mapel = [];
+        foreach ($list_mapel as $item => $value) {
+            $mapel[] = $value->nama_mapel;
+        }
+        $mapel = json_encode($mapel);
+
+        $id_kuri = Kelas::where('id_kelas', $id_kelas)->value('id_kuri');
+        $id_mapel = [];
+        foreach ($list_mapel as $item => $value) {
+            $id_mapel[] = $value->id_mapel;
+        }
+        foreach ($id_mapel as $item => $value) {
+            $nilai[] = Mapel::leftJoin('detnilai', 'mapel.id_mapel', '=', 'detnilai.id_mapel')->where('detnilai.id_mapel', '=', $value)->where('detnilai.id_kuri', '=', $id_kuri)->where('detnilai.id_kelas', '=', $id_kelas)->get();
+        }
+        $nilai_mapel = [];
+        foreach ($nilai as $item => $value) {
+            $nilai_mapel[] = $value->sum('nilai');
+        }
+        $jumlah_siswa = Siswa::where('id_kelas', $id_kelas)->count();
+        foreach ($nilai_mapel as $item => $value) {
+            $nilai_rata[] = $value / $jumlah_siswa;
+        }
+        $nilai_rata = json_encode($nilai_rata);
+        return view('dashboard2', compact('date', 'mapel', 'nilai_rata'));
     }
 }
--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -33,4 +33,67 @@
         </div>
     </div>
 </div>
+<div class="row"></div>
+<div class="card">
+    <div class="card-body">
+        <div class="chartjs-size-monitor">
+            <div class="chartjs-size-monitor-expand">
+                <div class=""></div>
+            </div>
+            <div class="chartjs-size-monitor-shrink">
+                <div class=""></div>
+            </div>
+        </div>
+        <h4 class="card-title">Grafik Nilai Rata-Rata Mapel</h4>
+        <canvas id="myChart"></canvas>
+    </div>
+</div>
+@endsection
+@section('script')
+<script>
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var mapel = {!! $mapel !!};
+    var nilai_rata = {!! $nilai_rata !!};
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: mapel,
+            datasets: [{
+                label: 'Nilai Rata-Rata',
+                data: nilai_rata,
+                backgroundColor: [
+                    'rgba(236, 236, 49, 0.5)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 156, 34, 0.5)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 239, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1,
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    type: 'linear',
+                }
+            }
+        }
+    });
+</script>
+<script>
+    setInterval(function () {
+        $('#date_time').load(' #date_time');
+    }, 60000);
+    </script>
 @endsection
